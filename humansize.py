8d4c3a3364de51ccb9b386bb1f72bc45646ee2c5
Lorenzo Di Stefano <lorenzodistefano.k@gmail.com>
2019-03-04 19:10:50 +0100

    adding comments

--- humansize.py
+++ humansize.py
@@ -1,6 +1,8 @@
 suffixes=["KB","MB","GB","TB","PB","EB","ZB","YB"]
 
 def approssimate_size(size):
+    """random comment"""
+    #multiline not working
     if(size<0):
         raise ValueError("number must be non-negative")
     multiple=1024
